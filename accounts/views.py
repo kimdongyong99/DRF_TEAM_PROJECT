424ca3666a12e969f37c2028fc160da58375efe5
kimdongyong99 <homeland007@naver.com>
2024-09-12 21:40:06 +0900

    프로필 수정 구현

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import User
+
 # from articles.models import Article
 from .serializers import Userserializers, UserProfileSerializer, UserChangeSerializer
 
@@ -11,21 +12,30 @@ from .serializers import Userserializers, UserProfileSerializer, UserChangeSeria
 class UserCreate(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = Userserializers
-    
-    
+
+
 class UserProfileView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def get(self, request, username):
         user = get_object_or_404(User, username=username)
         serializers = UserProfileSerializer(user)
         # articles = Article.objects.filter(author=user.pk)
         return Response(serializers.data)
-    
+
     def put(self, request, username):
+        new_password = request.data.get("new_password")
+        varify_password = request.data.get("varify_password")
+        if new_password != varify_password:
+            return Response({"error": "비밀번호가 일치하지 않습니다"}, status=400)
         user = get_object_or_404(User, username=username)
-        serializers = UserChangeSerializer(request.user, data= request.data, partial=True)
+        serializers = UserChangeSerializer(
+            request.user, data=request.data, partial=True
+        )
         if serializers.is_valid():
             serializers.save()
-            return Response(serializers.data, status=200)
-        
+            if new_password != "":
+                user.set_password(new_password)
+                user.save()
+            return Response(
+                {"detail": "정보수정이 완료되었습니다"}, status=status.HTTP_200_OK
+            )
