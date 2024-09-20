6a4a87e46398feadefb19ff950e2720f7b7f5b65
jung-sungwon <pompous89@gmail.com>
2024-09-20 09:13:11 +0900

    settings

--- Big_News/settings.py
+++ Big_News/settings.py
@@ -129,8 +129,10 @@ USE_TZ = True
 
 STATIC_URL = "static/default/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
