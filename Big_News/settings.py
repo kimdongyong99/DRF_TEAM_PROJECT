d43ce66a24f746ccdeee033e1310c0638058be17
jung-sungwon <pompous89@gmail.com>
2024-09-20 09:06:22 +0900

    settings

--- Big_News/settings.py
+++ Big_News/settings.py
@@ -17,6 +17,7 @@ from .my_settings import MY_SECRET_KEY
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -128,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/default/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
